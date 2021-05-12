ab9e972372d16c864bb6e0509ca166ec324c16dc
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-05-12 20:46:38 -0300

    Web Scraper | Stage 4

--- Medium/Web Scraper/Web Scraper/task/scraper.py
+++ Medium/Web Scraper/Web Scraper/task/scraper.py
@@ -1,17 +1,20 @@
 import requests
 import sys
 
+from bs4 import BeautifulSoup
+
 
 class Scraper:
     def __init__(self) -> None:
-        self.url = ""
+        self.url = "https://www.nature.com/nature/articles"
+        self.articles_link = {}
+        self.articles_content = {}
+        self.articles_saved = []
 
     def start_scraper(self) -> None:
-        self.get_input()
         self.make_request()
-
-    def get_input(self) -> None:
-        self.url = input("Input the URL:\n")
+        self.get_articles_content()
+        self.save_to_file()
 
     def make_request(self) -> None:
         r = requests.get(self.url)
@@ -19,14 +22,39 @@ class Scraper:
             print(f"\nThe URL returned {r.status_code}!")
             self.exit_program()
 
-        self.save_to_file(r.content)
+        self.get_articles_link(BeautifulSoup(r.content, "html.parser"))
 
-    @staticmethod
-    def save_to_file(page_content: dict) -> None:
-        with open("source.html", "wb") as f:
-            f.write(page_content)
+    def get_articles_link(self, soup: BeautifulSoup) -> None:
+        articles = soup.find_all("article", class_="c-card")
 
-        print("\nContent saved.")
+        for tags in articles:
+            span = tags.find("span", {"data-test": "article.type"}).get_text().strip()
+
+            if span == "News":
+                link = tags.find("a", {"class": "c-card__link"})
+                article_url = f'https://www.nature.com{link["href"]}'
+                article_title = link.get_text().strip().translate({ord(c): "_" for c in " ./?:'"})
+
+                self.articles_link.update({article_title: article_url})
+
+    def get_articles_content(self) -> None:
+        for k, v in self.articles_link.items():
+            r = requests.get(v)
+            soup = BeautifulSoup(r.content, "html.parser")
+            art_content = soup.find("div", class_="article__body")
+
+            if art_content:
+                self.articles_content.update({k: art_content.get_text().strip()})
+
+    def save_to_file(self) -> None:
+        for k, v in self.articles_content.items():
+            file_name = f"{k}.txt"
+            self.articles_saved.append(file_name)
+
+            with open(file_name, "wb") as f:
+                f.write(v.encode())
+
+        print("\nSaved articles:", self.articles_saved)
 
     @staticmethod
     def exit_program() -> None:
